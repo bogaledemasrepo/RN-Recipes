ec300b969b0e82dd3b42b412be5d9667b28d8b16
bogidemasrepo <bogidemas@gmail.com>
2026-01-23 11:55:31 +0300

    code clean up

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,48 +1,56 @@
 import React from "react";
 import {
   View,
-  Text,
-  FlatList,
-  ActivityIndicator,
   StyleSheet,
 } from "react-native";
 import RecipeCard from "../components/RecipeCard";
 import { useRecipeContext } from "../context/RecipeContext";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import EmptyState from "../components/ListEmpty";
-import { FlashList } from "@shopify/flash-list"
+import { FlashList } from "@shopify/flash-list";
 import FavListHeader from "../components/fav-header";
 
-const FavoritesScreen: React.FC<{navigation: any}> = ({navigation}:{navigation: any}) => {
-  const {top,bottom}=useSafeAreaInsets();
-  const { loading, favorites ,toggleFavorite} = useRecipeContext();
+const FavoritesScreen: React.FC<{ navigation: any }> = ({
+  navigation,
+}: {
+  navigation: any;
+}) => {
+  const { top, bottom } = useSafeAreaInsets();
+  const { loading, favorites, toggleFavorite } = useRecipeContext();
 
   const handleRecipePress = (item: any) => {
-    navigation.navigate('Detail', { recipe: item });
+    navigation.navigate("Detail", { recipe: item });
   };
 
   return (
-    <View style={[styles.screenContainer,{paddingTop:top,paddingBottom:bottom}]}>
-          <FavListHeader navigation={navigation}  />
-          <FlashList
-          showsVerticalScrollIndicator={false}
-          refreshing={loading}
-            data={favorites}
-            keyExtractor={(item) => item.idMeal}
-            renderItem={({ item }) => (
-              <RecipeCard
-                item={item}
-                toggleFavorite={toggleFavorite}
-                onPress={handleRecipePress}
-              />
-            )}
-            contentContainerStyle={styles.listContent}
-            ListEmptyComponent={()=><EmptyState title="No Favorite Found" description=""/>}
-            style={{ flex: 1 }}
+    <View
+      style={[
+        styles.screenContainer,
+        { paddingTop: top, paddingBottom: bottom },
+      ]}
+    >
+      <FavListHeader navigation={navigation} />
+      <FlashList
+        showsVerticalScrollIndicator={false}
+        refreshing={loading}
+        data={favorites}
+        keyExtractor={(item) => item.idMeal}
+        renderItem={({ item }) => (
+          <RecipeCard
+            item={item}
+            toggleFavorite={toggleFavorite}
+            onPress={handleRecipePress}
           />
+        )}
+        contentContainerStyle={styles.listContent}
+        ListEmptyComponent={() => (
+          <EmptyState title="No Favorite Found" description="" />
+        )}
+        style={{ flex: 1 }}
+      />
     </View>
   );
-};  
+};
 
 const styles = StyleSheet.create({
   screenContainer: {
