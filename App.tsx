0c071129ddb7087a1cbea8810b37ac63703b35db
bogidemasrepo <bogidemas@gmail.com>
2026-02-02 03:42:42 +0300

    add spashscreen pkg

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppNavigator from "./AppNavigator";
 import RecipesProvider from "./context/RecipeContext";
 
+
 const App = () => {
+  // Keep the splash screen visible while we fetch resources
+
+
   return (
     <RecipesProvider>
       <AppNavigator />
